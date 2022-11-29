cebf0c1b125c5dd4843debb0adef35c6eb03932d
Patch <patchfact@gmail.com>
2022-11-29 13:01:21 -0800

    added temp button to transactions

--- BudgetingApp/app/src/main/java/com/example/budgetingapp/MainActivity.java
+++ BudgetingApp/app/src/main/java/com/example/budgetingapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.budgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button transaction_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        transaction_button = findViewById(R.id.transactions_button);
+        transaction_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TransactionsOverview.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ BudgetingApp/app/src/main/java/com/example/budgetingapp/TransactionsOverview.java
@@ -0,0 +1,14 @@
+package com.example.budgetingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransactionsOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transactions_overview);
+    }
+}
