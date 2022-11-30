e94d1f42408977775357e867b796c63c5e8e5e71
Patch <patchfact@gmail.com>
2022-11-29 19:24:11 -0800

    began work on transactions

--- BudgetingApp/app/src/main/java/com/example/budgetingapp/TransactionsOverview.java
+++ BudgetingApp/app/src/main/java/com/example/budgetingapp/TransactionsOverview.java
@@ -2,13 +2,32 @@ package com.example.budgetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TransactionsOverview extends AppCompatActivity {
+
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions_overview);
+
+        getSupportActionBar().setTitle("Transactions");
+
+        addButton = findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TransactionsOverview.this, AddTransaction.class);
+
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
